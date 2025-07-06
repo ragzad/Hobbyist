c2c30cb18cf5940f0ac351434db4cdfab3f77bcd
ethan davies <edavies9000@gmail.com>
2025-07-06 23:55:07 +0100

    Implement list and detail views for projects

--- core/apps/projects/views.py
+++ core/apps/projects/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Project
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+# A view to display a list of all projects belonging to the logged-in user
+class ProjectListView(LoginRequiredMixin, ListView):
+    model = Project
+    template_name = 'projects/project_list.html' # We will create this file next
+    context_object_name = 'projects'
+
+    def get_queryset(self):
+        # This ensures users only ever see their own projects
+        return Project.objects.filter(owner=self.request.user)
+
+# A view to display the details of a single project
+class ProjectDetailView(LoginRequiredMixin, DetailView):
+    model = Project
+    template_name = 'projects/project_detail.html' # We will create this file next
+    context_object_name = 'project'
+
+    def get_queryset(self):
+        # This ensures a user can only see the detail page of their own project
+        return Project.objects.filter(owner=self.request.user)
