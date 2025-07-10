56d83194100c8d94995cf5dfd887252d88bba32d
ethan davies <edavies9000@gmail.com>
2025-07-10 19:27:13 +0100

    Update production.py

--- core/core/settings/production.py
+++ core/core/settings/production.py
@@ -1,6 +1,6 @@
+# core/core/settings/production.py
 from .base import *
 
 DEBUG = False
-
-# TODO: Add your live website domain here before deploying
-# ALLOWED_HOSTS = ['your-app-name.herokuapp.com']
+ALLOWED_HOSTS = ['https://git.heroku.com/hobb.git']
+STATIC_ROOT = BASE_DIR.parent.parent / 'staticfiles'
