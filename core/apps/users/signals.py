60ae90cb03d532f3b35c96919c6bfe31cb6b67e0
ethan davies <edavies9000@gmail.com>
2025-07-11 00:40:57 +0100

    Update signals.py

--- core/apps/users/signals.py
+++ core/apps/users/signals.py
@@ -1,14 +1,9 @@
-# apps/users/signals.py
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
 
 @receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    """
-    Create a profile for a new user, or just save the profile for an existing user.
-    """
+def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-    instance.profile.save()
