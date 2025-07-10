2de7fcd86100d270587023722c2432c2ef9c9d18
ethan davies <edavies9000@gmail.com>
2025-07-10 18:52:59 +0100

    Fix project structure

--- manage.py
+++ manage.py
@@ -3,10 +3,10 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.development')
+    # Point to the production settings file for deployment
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.core.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,7 +16,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
