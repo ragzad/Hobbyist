99049c121506701e59d2e256ef10c8017034530b
ethan davies <edavies9000@gmail.com>
2025-07-10 23:32:49 +0100

    Update production.py

--- core/core/settings/production.py
+++ core/core/settings/production.py
@@ -1,3 +1,5 @@
+# core/settings/production.py
+
 import os
 import dj_database_url
 from .base import *
@@ -7,22 +9,25 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
 
+# NOTE: For a real production app you would lock this down further.
+ALLOWED_HOSTS = ['*']
+
+
+CSRF_TRUSTED_ORIGINS = ['https://hobb-5e258a5700a4.herokuapp.com']
+
+
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
-
-# Static files with WhiteNoise
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
+# Static files settings for production
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MIDDLEWARE += [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-]
-
-# CSRF settings for production
-CSRF_TRUSTED_ORIGINS = ['https://*.herokuapp.com']
+# Add WhiteNoise to middleware
+# Make sure it's placed after SecurityMiddleware
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
